2503cc8c8908c81eeb842b5d4a836fb811ca896e
benjyrr <31515007+benjyrr@users.noreply.github.com>
2017-10-20 08:57:59 -0700

    ddd

--- main.js
+++ main.js
@@ -1,10 +1,23 @@
-
+var list =["We shall see!",
+			"Yes!",
+			"No!",
+			"Maybe.",
+			"Certainly.",
+			"Ask again later!",
+			"Doubtful",
+			"Try again later.",
+			"As I see it, yes.",
+			"Signs point to yes!"];
 
 function testJs(){
 	alert("test!");
 }
 
 function shake8ball(){
-	document.getElementById("magic8ball").src="giphy.gif"
+	document.getElementById("magic8ball").src="giphy.gif";
 	alert("We shall see!");
+
+	document.getElementById("results").innerhtml="We shall see!";
+
+	alert(list[2]);
 }
