2a6d2c441bfd44b886152db9bb5849985cee97eb
iTzDreamZ <31861247+Siddthecoder35@users.noreply.github.com>
2017-12-01 08:54:32 -0800

    changes

--- main.js
+++ main.js
@@ -36,3 +36,16 @@ Below is the code for Tic Tac Toe
 function turn(location){
 	document.getElementById(location).innerHTML = "X";
 }
+
+//clears X's and O's
+function clearALL(){
+	document.getElementById('r1c1').innerHTML = ""
+	document.getElementById('r1c2').innerHTML = ""
+	document.getElementById('r1c3').innerHTML = ""
+	document.getElementById('r2c1').innerHTML = ""
+	document.getElementById('r2c2').innerHTML = ""
+	document.getElementById('r2c3').innerHTML = ""
+	document.getElementById('r3c1').innerHTML = ""
+	document.getElementById('r3c2').innerHTML = ""
+	document.getElementById('r3c3').innerHTML = ""
+}
