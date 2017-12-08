23139b2db74c36baf4238608d061e7e9cdd59309
Johns <csen@newroads.org>
2017-12-08 09:17:25 -0800

    i need to fix this

--- main.js
+++ main.js
@@ -38,6 +38,9 @@ var character= "O";
 
 function turn(location){
 
+//if no X or O, then draw
+if(document.getElementById(location).innerHTML == ""){
+//switch between X and O
 	if(character == "X"){
 		character = "O";
 	}
@@ -46,17 +49,48 @@ function turn(location){
 	}
 
 	document.getElementById(location).innerHTML = character;
+	}
+
+	checkWinner();
 }
 
 //clears X's and O's
 function clearALL(){
-	document.getElementById ("r1c1").innerHTML = ""
-	document.getElementById ("r1c2").innerHTML = ""
-	document.getElementById ("r1c3").innerHTML = ""
-	document.getElementById ("r2c1").innerHTML = ""
-	document.getElementById ("r2c2").innerHTML = ""
-	document.getElementById ("r2c3").innerHTML = ""
-	document.getElementById ("r3c1").innerHTML = ""
-	document.getElementById ("r3c2").innerHTML = ""
-	document.getElementById ("r3c3").innerHTML = ""
+	document.getElementById ("r1c1").innerHTML = "";
+	document.getElementById ("r1c2").innerHTML = "";
+	document.getElementById ("r1c3").innerHTML = "";
+	document.getElementById ("r2c1").innerHTML = "";
+	document.getElementById ("r2c2").innerHTML = "";
+	document.getElementById ("r2c3").innerHTML = "";
+	document.getElementById ("r3c1").innerHTML = "";
+	document.getElementById ("r3c2").innerHTML = "";
+	document.getElementById ("r3c3").innerHTML = "";
 }
+
+
+function checkWinner(){
+	var r1c1 = document.getElementById ("r1c1").innerHTML; 
+	var r1c2 = document.getElementById ("r1c2").innerHTML; 
+	var r1c3 = document.getElementById ("r1c3").innerHTML; 
+	var r2c1 = document.getElementById ("r2c1").innerHTML; 
+	var r2c2 = document.getElementById ("r2c2").innerHTML;
+	var r2c3 = document.getElementById ("r2c3").innerHTML; 
+	var r3c1 = document.getElementById ("r3c1").innerHTML; 
+	var r3c2 = document.getElementById ("r3c2").innerHTML; 
+	var r3c3 = document.getElementById ("r3c3").innerHTML; 
+
+	if(r1c1 == r1c2 && r1c1 == r1c3 && r1c1 !=""||
+		r2c1 == r2c2 && r2c1 ==r2c3 && r2c1 !=""||
+		r3c1 == r3c2 && r3c1 ==r3c3 && r3c1 !=""||){
+
+		alert("Winner!");
+	}
+
+}
+
+
+
+
+
+
+
