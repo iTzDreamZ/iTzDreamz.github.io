890e600973df9d987cb1cbd217aa8914de3270e1
kingbrouno7 <31861268+kingbruno7@users.noreply.github.com>
2018-01-26 09:40:01 -0800

    work friday

---
+++ flashcards.js
@@ -0,0 +1,17 @@
+
+//addition operation
+function addNumbers(){
+
+	//get first number of textbox
+	var operand1 = document.getElementById("operand1").value;
+	var operand2 = document.getElementById("operand2").value;
+
+
+	//add to second number of second textbox
+	var sum = parseInt(operand1) + parseInt(operand2);
+
+	//test sum
+	alert(sum);
+
+	//check if the sum equals the answer textbox
+}
